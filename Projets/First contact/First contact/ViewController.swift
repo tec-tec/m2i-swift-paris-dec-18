81d5b67ce2ef1874d438b8fb37aa951ae173f226
Ludovic Ollagnier <ludovic@ollagnier.me>
2018-12-12 21:34:22 +0100

    Outlet / actions and save

--- Projets/First contact/First contact/ViewController.swift
+++ Projets/First contact/First contact/ViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let library = Library()
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var authorTextField: UITextField!
+    @IBOutlet weak var pagesTextField: UITextField!
+    @IBOutlet weak var isbnTextField: UITextField!
+
+    @IBOutlet weak var isDigitalSwitch: UISwitch!
+    @IBOutlet weak var isReadSwitch: UISwitch!
+    @IBOutlet weak var notationSlider: UISlider!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
     }
 
+    @IBAction func readValueChanged(_ sender: UISwitch) {
 
+        notationSlider.isHidden = !sender.isOn
+    }
+
+    @IBAction func save(_ sender: UIButton) {
+
+        guard let author = authorTextField.text, author.count > 0 else { return }
+        guard let title = titleTextField.text, title.count > 0 else { return }
+        guard let nbPagesString = pagesTextField.text, let nbPagesInt = Int(nbPagesString)  else { return }
+        guard let isbn = isbnTextField.text, isbn.count > 0 else { return }
+
+        var note: Float? = nil
+        if isReadSwitch.isOn {
+            note = notationSlider.value
+        }
+
+        let newBook = Book(author: author, title: title, nbOfPages: nbPagesInt, isbn: isbn, isDigital: isDigitalSwitch.isOn, note: note)
+        library.add(newBook)
+    }
 }
 
